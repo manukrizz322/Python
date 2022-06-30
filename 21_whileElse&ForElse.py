50d3cc8eb50cde29057db44f9beb6ba8fb4ada87
manukrizz322 <manukrizz322@gmail.com>
2022-06-30 17:22:35 +0530

    1st half complted

---
+++ 21_whileElse&ForElse.py
@@ -0,0 +1,20 @@
+# While Else & For Else
+i=1
+while i<=5:
+    #if(i==4):
+       # break
+    print(i)
+    i+=1
+else:
+    print("Loop Completed")
+
+for i in range(1,21):
+    if(i==2):
+        continue
+    if(i==5):
+        break
+    print(i)
+else:
+    print("For Loop Completed")    
+
+    
